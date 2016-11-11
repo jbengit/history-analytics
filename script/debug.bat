125b1c44b41c6102a8bac51f2fd73379e678241a
Joel Benchitrit <joelb@tlv.lpnet.com>
2016-11-11 15:52:52 +0200

    initial commit

---
+++ script/debug.bat
@@ -0,0 +1,2 @@
+spark-submit --master local --deploy-mode client --class com.consumer.Main --name history-analytics  ../target/history-analytics-1.0-SNAPSHOT.jar
+--driver-java-options -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005
---
+++ src/main/scala/com/consumer/Main.scala
@@ -0,0 +1,23 @@
+package com.consumer
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
+
+object Main {
+  
+  def main(args: Array[String])
+  {
+    val conf = new SparkConf().setAppName("Word Counter");
+    conf.setMaster("local")
+    val sc = new SparkContext(conf);
+       
+    val textFile = sc.textFile("file:///C:/tmp/data/output/RFM/part-r-00000", 1)
+    val tokenizedFileData = textFile.flatMap { line => line.split(" ") }
+    val countPrep = tokenizedFileData.map ( word => (word, 1) )
+    val counts = countPrep.reduceByKey((accumValue, newValue) => accumValue + newValue)
+    val sortedCounts = counts.sortBy(kvPair => kvPair._2, false)
+    sortedCounts.saveAsTextFile("file:///C:/tmp/data/spark-output/RFM/ReadMeWordCountViaApp")
+  }
+  
+}
